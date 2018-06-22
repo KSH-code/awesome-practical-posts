f971aabc32475ebadcc0cbac84d517fa3d91a35b
ksh-code <tjdgnsqn133@gmail.com>
2018-06-22 18:07:48 +0900

    What is 'else if' in js

--- js/README.md
+++ js/README.md
@@ -2,3 +2,4 @@
 * [Lexical Scope 에 대해서 이해하고 있는가?](lexical-scope/)
 * [Promise 객체는 어떻게 에러 핸들링을 하고 있는가?](promise-error-handling/)
 * [Bitwise operators 는 어떻게 작동하는가?](bitwise-operators/)
+* [else if 란 무엇인가?](else-if/)
---
+++ js/else-if/result.js
@@ -0,0 +1,12 @@
+const condition = true
+if (condition) {
+
+} else if () {
+
+} else if () {
+
+}
+
+// 일단 충격적이게도 js 에는 else if 라는 키워드가 존재하지 않는다.
+// 실행문이 하나인 경우 {} 를 생략해도 되기 때문에 
+// if { } else { if { } else { if }} 와 같이 선언되는것과 동일하다.
